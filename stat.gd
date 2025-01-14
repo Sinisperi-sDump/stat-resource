6276003221879b72827f24af1ea97b7cbdd1191e
GildedBirch <150861088+GildedBirch@users.noreply.github.com>
2025-01-14 17:15:53 +0200

    Added main file

---
+++ stat.gd
@@ -0,0 +1,54 @@
+extends Resource
+## Stat 1.0 [br]
+## By GildedBirch
+class_name Stat
+
+## Emitted when value_total becomes 0
+signal value_zero
+## Emitted when value, temp, max, or max_bonus gets updated [br]
+## Good for updating UI
+signal value_changed
+
+## Current value of stat
+var value: float = 0.0:
+	set(value_in):
+		value = clampf(value_in, 0.0, max_total())
+		value_changed.emit()
+		if current_total() <= 0.0: value_zero.emit()
+
+## Base max value if stat
+@export var max_value: float = 1.0:
+	set(value_in):
+		max_value = max(0.0, value_in)
+		if max_total() < value:
+			value = max_total()
+		else:
+			value_changed.emit()
+
+## Bonus to max value of stat
+@export var max_bonus: float = 0.0:
+	set(value_in):
+		max_bonus = max(0.0, value_in)
+		if max_total() < value:
+			value = max_total()
+		else:
+			value_changed.emit()
+
+## Temporary increase in stat. Can go over max_total
+@export var temp: float = 0.0:
+	set(value_in):
+		temp = max(0.0, value_in)
+		value_changed.emit()
+		if current_total() <= 0.0: value_zero.emit()
+
+## Total of maxium stat with bonus
+func max_total() -> float:
+	return max_value + max_bonus
+
+## Total of current value with temp
+func current_total() -> float:
+	return value + temp
+
+## Print for debug purposes
+func _print_stat():
+	print("%s(%s+%s) / %s(%s+%s)" % [current_total(), value, temp, max_total(), max_value, max_bonus])
